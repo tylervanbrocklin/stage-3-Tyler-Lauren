c4bafbfd12173d56dfb6bceef05e1201f3ac1900
tylervanbrocklin <tylerva@uw.edu>
2018-11-26 23:16:20 -0800

    Updates

---
+++ precache-manifest.d9926b3976065e09439bb25ed6e487af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f71120a25d89dd30de374ab739550321",
+    "url": "/stage-3-Tyler-Lauren/static/media/uwhousing.f71120a2.png"
+  },
+  {
+    "revision": "256cf2331bd1f8d240e9daa852404e88",
+    "url": "/stage-3-Tyler-Lauren/static/media/uw_quad.256cf233.png"
+  },
+  {
+    "revision": "a2bec6fc40565a6acd76e9a7889589ad",
+    "url": "/stage-3-Tyler-Lauren/static/media/uw-alder.a2bec6fc.jpg"
+  },
+  {
+    "revision": "2a68b1edc36324c575fe",
+    "url": "/stage-3-Tyler-Lauren/static/js/runtime~main.2a68b1ed.js"
+  },
+  {
+    "revision": "7c8fe5c94f8a7973dd08",
+    "url": "/stage-3-Tyler-Lauren/static/js/main.7c8fe5c9.chunk.js"
+  },
+  {
+    "revision": "7518dc498d985ec3b255",
+    "url": "/stage-3-Tyler-Lauren/static/js/1.7518dc49.chunk.js"
+  },
+  {
+    "revision": "7c8fe5c94f8a7973dd08",
+    "url": "/stage-3-Tyler-Lauren/static/css/main.639a464c.chunk.css"
+  },
+  {
+    "revision": "7518dc498d985ec3b255",
+    "url": "/stage-3-Tyler-Lauren/static/css/1.47596027.chunk.css"
+  },
+  {
+    "revision": "386e96cd38ff46945574cada3740492e",
+    "url": "/stage-3-Tyler-Lauren/index.html"
+  }
+];
